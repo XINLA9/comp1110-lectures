184339d55761fea343068e32bcea4854229e025c
gbarca <giuseppemj.barca@gmail.com>
2022-07-25 22:51:35 +1000

    added J02

---
+++ src/comp1110/lectures/J02/BasicImperative.java
@@ -0,0 +1,15 @@
+package comp1110.lectures.J02;
+
+public class BasicImperative {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = 2;
+        System.out.println(i);
+        if (i > 3) {
+            System.out.println("Greater than 300");
+        }
+    }
+}
