ff38a91acf86afa90cd7ed99bec5a64cf6f02395
gbarca <giuseppemj.barca@gmail.com>
2022-08-11 11:59:24 +1000

    added O03, J09, O04

---
+++ src/comp1110/lectures/O04/Mammal.java
@@ -0,0 +1,11 @@
+package comp1110.lectures.O04;
+
+public class Mammal extends Animal {
+    public Mammal(String name) {
+        super(name);
+    }
+
+    public static double getNumberOfSpecies() {
+        return 1e6;
+    }
+}
